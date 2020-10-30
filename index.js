0be0cfaf5844f7a974ce76ae0a3354a0b7481bdd
João Roberto Lopes da Silva <joao.r.lopesdasilva@gmail.com>
2020-10-29 23:48:07 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const {Client} = require('discord.js');
+const client = new Client();
+const BOT_SECRET = process.env.BOT_SECRET;
+const prefix = '!'
+
+client.on("ready", () => {
+    console.log(`Bot ${client.user.tag} online.`);
+});
+
+client.on("error", console.error);
+
+client.on("message", async message => {
+    if(message.content === "!russiankid"){
+        setInterval( interval => {
+            message
+                .channel
+                    .send("https://tenor.com/view/kid-meme-dancing-37-nation-gif-5514765");
+        }, 10 * 10000);
+    }
+})
+
+client.login(BOT_SECRET);
